e85fec1a6104f75570d1a3ddc1490edccc9ad6be
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-02-24 08:40:34 -1000

    task 1

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -8,3 +8,19 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+@app.route("/api/get-lucky-num", methods=['POST'])
+def lucky_num():
+    """Define JSON body with name,email,year,color"""
+    name = request.json["name"]
+    email = request.json["email"]
+    year = request.json["year"]
+    color = request.json["color"]
+
+    req = {
+        "name": name,
+        "email": email,
+        "year": year,
+        "color": color
+    }
+
